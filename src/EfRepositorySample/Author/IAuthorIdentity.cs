8f5779d6b4654411e9080ce9d66091d64da5f63e
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-13 22:12:39 +0300

    add author interfaces

---
+++ src/EfRepositorySample/Author/IAuthorIdentity.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace EfRepositorySample.Author
+{
+  /// <summary>Represents an author identity.</summary>
+  public interface IAuthorIdentity
+  {
+  }
+}

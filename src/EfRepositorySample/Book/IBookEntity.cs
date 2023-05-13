acada07ef6d936c57b9b199a625c8c7c35405980
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-13 22:15:00 +0300

    add book interfaces

---
+++ src/EfRepositorySample/Book/IBookEntity.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace EfRepositorySample.Book
+{
+  /// <summary>Represents a book entity.</summary>
+  public interface IBookEntity
+  {
+  }
+}
---
+++ src/EfRepositorySample/Book/IBookRepository.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace EfRepositorySample.Book
+{
+  /// <summary>Provides a simple API to persistence of the <see cref="EfRepositorySample.Book.IBookEntity"/>.</summary>
+  public interface IBookRepository
+  {
+  }
+}

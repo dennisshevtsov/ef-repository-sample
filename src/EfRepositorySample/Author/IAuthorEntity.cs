9f547bda03dd44c4756b96652f751f576d34d95b
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-13 22:22:08 +0300

    add author entity

--- src/EfRepositorySample/Author/IAuthorEntity.cs
+++ src/EfRepositorySample/Author/IAuthorEntity.cs
@@ -5,7 +5,12 @@
 namespace EfRepositorySample.Author
 {
   /// <summary>Represents an author entity.</summary>
-  public interface IAuthorEntity
+  public interface IAuthorEntity : IAuthorIdentity
   {
+    /// <summary>Gets an object that represents a name of an author.</summary>
+    public string Name { get; }
+
+    /// <summary>Gets an object that represents a bio of an author.</summary>
+    public string Bio { get; set; }
   }
 }

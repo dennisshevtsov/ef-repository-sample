e7a21773131f75efbf419a0180229902ba171e69
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-14 19:49:55 +0300

    implement delete

--- src/EfRepositorySample.Data/RepositoryBase.cs
+++ src/EfRepositorySample.Data/RepositoryBase.cs
@@ -2,10 +2,10 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
-using Microsoft.EntityFrameworkCore;
-
 namespace EfRepositorySample.Data
 {
+  using Microsoft.EntityFrameworkCore;
+
   /// <summary>Provides a simple API to persistence of an entity.</summary>
   public abstract class RepositoryBase<TEntityImpl, TEntity, TIdentity> : IRepository<TEntity, TIdentity>
     where TEntityImpl : EntityBase, TEntity
@@ -84,7 +84,11 @@ namespace EfRepositorySample.Data
     /// <returns>An object that represents an asynchronous operation.</returns>
     public Task DeleteAsync(TIdentity identity, CancellationToken cancellationToken)
     {
-      throw new NotImplementedException();
+      var id = EntityBase.Create(identity).Id;
+
+      return DbContext.Set<TEntityImpl>()
+                      .Where(entity => entity.Id == id)
+                      .ExecuteDeleteAsync(cancellationToken);
     }
   }
 }

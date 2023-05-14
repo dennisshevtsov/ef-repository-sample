21c91bc7651396a860db408309f8199b74e6080a
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-15 01:01:53 +0300

    data services extensions

---
+++ src/EfRepositorySample.Data/DataServicesExtensions.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+  using EfRepositorySample.Author;
+  using EfRepositorySample.Book;
+  using EfRepositorySample.Data.Author;
+  using EfRepositorySample.Data.Book;
+
+  /// <summary>Extends an API of the <see cref="Microsoft.Extensions.DependencyInjection.IServiceCollection"/>.</summary>
+  public static class DataServicesExtensions
+  {
+    /// <summary>Registers infrastructure services.</summary>
+    /// <param name="services">An object that specifies the contract for a collection of service descriptors.</param>
+    /// <returns>An object that specifies the contract for a collection of service descriptors.</returns>
+    public static IServiceCollection AddData(this IServiceCollection services)
+    {
+      services.AddRepositories();
+
+      return services;
+    }
+
+    /// <summary>Registers infrastructure services.</summary>
+    /// <param name="services">An object that specifies the contract for a collection of service descriptors.</param>
+    /// <returns>An object that specifies the contract for a collection of service descriptors.</returns>
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+      services.AddScoped<IAuthorRepository, AuthorRepository>();
+      services.AddScoped<IBookRepository, BookRepository>();
+
+      return services;
+    }
+  }
+}

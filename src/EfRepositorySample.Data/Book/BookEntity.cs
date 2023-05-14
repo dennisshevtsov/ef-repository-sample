429f6f16a0ba7130c3f2898a6b9fb4a0b127eb2d
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-14 19:04:12 +0300

    implement book

---
+++ src/EfRepositorySample.Data/Book/BookEntity.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace EfRepositorySample.Data.Book
+{
+  using EfRepositorySample.Book;
+
+  /// <summary>Represents a book entity.</summary>
+  public sealed class BookEntity : IBookEntity
+  {
+    /// <summary>Initializes a new instance of the <see cref="EfRepositorySample.Data.Book.BookEntity"/> class.</summary>
+    public BookEntity()
+    {
+      Title       = string.Empty;
+      Description = string.Empty;
+    }
+
+    /// <summary>Initializes a new instance of the <see cref="EfRepositorySample.Data.Book.BookEntity"/> class.</summary>
+    /// <param name="bookEntity">An object that represents a book entity.</param>
+    public BookEntity(IBookEntity bookEntity) : this()
+    {
+      BookId      = bookEntity.BookId;
+      Title       = bookEntity.Title;
+      Description = bookEntity.Description;
+      Pages       = bookEntity.Pages;
+    }
+
+    /// <summary>Gets an object that represents an ID of a book.</summary>
+    public Guid BookId { get; }
+
+    /// <summary>Gets an object that represents a title of a book.</summary>
+    public string Title { get; }
+
+    /// <summary>Gets an object that represents a description of a book.</summary>
+    public string Description { get; }
+
+    /// <summary>Gets an object that represents a number of pages of a book.</summary>
+    public int Pages { get; }
+  }
+}

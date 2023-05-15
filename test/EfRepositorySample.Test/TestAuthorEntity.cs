cb67427585c2229da7a97ae027af5d3a3780a557
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-16 00:10:29 +0300

    add test author entity

---
+++ test/EfRepositorySample.Test/TestAuthorEntity.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace EfRepositorySample.Test
+{
+  using EfRepositorySample.Author;
+
+  public sealed class TestAuthorEntity : IAuthorEntity
+  {
+    public TestAuthorEntity(string name, string bio)
+    {
+      Name = name ?? throw new ArgumentNullException(nameof(name));
+      Bio  = bio  ?? throw new ArgumentNullException(nameof(bio));
+    }
+
+    public TestAuthorEntity(Guid authorId, string name, string bio) : this(name, bio)
+    {
+      AuthorId = authorId;
+    }
+
+    public Guid AuthorId { get; }
+
+    public string Name { get; }
+
+    public string Bio { get; }
+  }
+}

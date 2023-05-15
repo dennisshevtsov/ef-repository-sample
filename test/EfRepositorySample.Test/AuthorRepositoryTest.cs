862e5a4dcbed7d092a74ac91bdfb490a404b3232
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-16 00:11:52 +0300

    add author repository test

---
+++ test/EfRepositorySample.Test/AuthorRepositoryTest.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace EfRepositorySample.Test
+{
+  using Microsoft.Extensions.DependencyInjection;
+
+  using EfRepositorySample.Author;
+
+  [TestClass]
+  public sealed class AuthorRepositoryTest : IntegrationTestBase
+  {
+#pragma warning disable CS8618
+    private IAuthorRepository _authorRepository;
+#pragma warning restore CS8618
+
+    protected override void Initialize(IServiceProvider provider)
+    {
+      _authorRepository = provider.GetRequiredService<IAuthorRepository>();
+    }
+  }
+}

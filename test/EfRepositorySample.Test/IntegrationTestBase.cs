671808083865ea3daa02b942c4a4bb627ebf8737
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-16 00:11:20 +0300

    add integration test base

---
+++ test/EfRepositorySample.Test/IntegrationTestBase.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace EfRepositorySample.Test
+{
+  using Microsoft.EntityFrameworkCore;
+  using Microsoft.Extensions.Configuration;
+  using Microsoft.Extensions.DependencyInjection;
+
+  public abstract class IntegrationTestBase
+  {
+#pragma warning disable CS8618
+    private DbContext _dbContext;
+    private IServiceScope _serviceScope;
+#pragma warning restore CS8618
+
+    [TestInitialize]
+    public void Initialize()
+    {
+      var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json")
+                                                    .Build();
+
+      _serviceScope = new ServiceCollection().AddData(configuration)
+                                             .BuildServiceProvider()
+                                             .CreateScope();
+
+      _dbContext = _serviceScope.ServiceProvider.GetRequiredService<DbContext>();
+      _dbContext.Database.EnsureCreated();
+
+      Initialize(_serviceScope.ServiceProvider);
+    }
+
+    protected abstract void Initialize(IServiceProvider provider);
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+      _dbContext?.Database?.EnsureDeleted();
+      _serviceScope?.Dispose();
+    }
+
+    protected DbContext DbContext => _dbContext;
+  }
+}

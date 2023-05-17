eefd0e592c8605fdb0b29b29dc1a2388d62d1bdc
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-18 01:25:59 +0300

    add book repository test

---
+++ test/EfRepositorySample.Test/Book/BookRepositoryTest.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace EfRepositorySample.Test.Book
+{
+  using Microsoft.Extensions.DependencyInjection;
+
+  using EfRepositorySample.Book;
+
+  [TestClass]
+  public sealed class BookRepositoryTest : IntegrationTestBase
+  {
+#pragma warning disable CS8618
+    private IBookRepository _bookRepository;
+#pragma warning restore CS8618
+
+    protected override void Initialize(IServiceProvider provider)
+    {
+      _bookRepository = provider.GetRequiredService<IBookRepository>();
+    }
+  }
+}

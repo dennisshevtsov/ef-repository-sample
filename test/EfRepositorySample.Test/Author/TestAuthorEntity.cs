071de9c6211f6f01d95a8528d9eee7a91ea865db
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-20 19:45:41 +0300

    add test method to test author entity

--- test/EfRepositorySample.Test/Author/TestAuthorEntity.cs
+++ test/EfRepositorySample.Test/Author/TestAuthorEntity.cs
@@ -33,5 +33,38 @@ namespace EfRepositorySample.Test.Author
     public string Bio { get; }
 
     public IEnumerable<IBookEntity> Books { get; }
+
+    public static TestAuthorEntity New() => new TestAuthorEntity(
+      Guid.NewGuid().ToString(),
+      Guid.NewGuid().ToString(),
+      new List<IBookEntity>());
+
+    public static void AreEqual(
+      IEnumerable<IAuthorEntity> controlAuthorEntityCollection,
+      IEnumerable<IAuthorEntity> actualAuthorEntityCollection)
+    {
+      var controlAuthorEntityList =
+        controlAuthorEntityCollection.OrderBy(entity => entity.AuthorId)
+                                   .ToList();
+
+      var actualAuthorEntityList =
+        actualAuthorEntityCollection.OrderBy(entity => entity.AuthorId)
+                                  .ToList();
+
+      Assert.AreEqual(controlAuthorEntityList.Count, actualAuthorEntityList.Count);
+
+      for (int i = 0; i < controlAuthorEntityList.Count; i++)
+      {
+        TestAuthorEntity.AreEqual(controlAuthorEntityList[i], actualAuthorEntityList[i]);
+      }
+    }
+
+    public static void AreEqual(
+      IAuthorEntity controlAuthorEntity,
+      IAuthorEntity actualAuthorEntity)
+    {
+      Assert.AreEqual(controlAuthorEntity.Name, actualAuthorEntity.Name);
+      Assert.AreEqual(controlAuthorEntity.Bio, actualAuthorEntity.Bio);
+    }
   }
 }

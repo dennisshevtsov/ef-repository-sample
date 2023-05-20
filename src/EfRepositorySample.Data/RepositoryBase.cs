280057be53e1807b17610c7b81def785d0298dd9
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-20 19:59:15 +0300

    fix add related entities

--- src/EfRepositorySample.Data/RepositoryBase.cs
+++ src/EfRepositorySample.Data/RepositoryBase.cs
@@ -46,6 +46,18 @@ namespace EfRepositorySample.Data
       var dbEntityEntry = DbContext.Entry(dbEntity);
 
       dbEntityEntry.State = EntityState.Added;
+
+      foreach (var collectionEntry in dbEntityEntry.Collections)
+      {
+        if (collectionEntry.CurrentValue != null)
+        {
+          foreach (var collectionItemEntity in collectionEntry.CurrentValue)
+          {
+            DbContext.Entry(collectionItemEntity).State = EntityState.Unchanged;
+          }
+        }
+      }
+
       await DbContext.SaveChangesAsync(cancellationToken);
       dbEntityEntry.State = EntityState.Detached;
 

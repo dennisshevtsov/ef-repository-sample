87a665fced02e72b9b961d5a4774cfb693600391
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-23 23:25:00 +0300

    add updatable interface

--- src/EfRepositorySample.Data/IUpdatable.cs
+++ src/EfRepositorySample.Data/IUpdatable.cs
@@ -5,11 +5,16 @@
 namespace EfRepositorySample.Data
 {
   /// <summary>Provides a simple API to update an entity.</summary>
-  /// <typeparam name="TEntity">A type of an entity from that this entity is updated.</typeparam>
+  /// <typeparam name="TEntity">A type of an entity from which this entity should be updated.</typeparam>
   public interface IUpdatable<TEntity>
   {
     /// <summary>Updates this entity.</summary>
-    /// <param name="entity">An object that represents an entity from that this entity is updated.</param>
-    public void Update(TEntity entity);
+    /// <param name="newEntity">An object that represents an entity from which this entity should be updated.</param>
+    public void Update(TEntity newEntity);
+
+    /// <summary>Updates this entity.</summary>
+    /// <param name="newEntity">An object that represents an entity from which this entity should be updated.</param>
+    /// <param name="properties">An object that represents a collection of properties to update.</param>
+    public void Update(TEntity newEntity, IEnumerable<string> properties);
   }
 }

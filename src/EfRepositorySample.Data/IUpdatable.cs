64876a963c228a20c70058fd3d638be88f872d79
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-23 23:22:49 +0300

    add updatable interface

---
+++ src/EfRepositorySample.Data/IUpdatable.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace EfRepositorySample.Data
+{
+  /// <summary>Provides a simple API to update an entity.</summary>
+  /// <typeparam name="TEntity">A type of an entity from that this entity is updated.</typeparam>
+  public interface IUpdatable<TEntity>
+  {
+    /// <summary>Updates this entity.</summary>
+    /// <param name="entity">An object that represents an entity from that this entity is updated.</param>
+    public void Update(TEntity entity);
+  }
+}
